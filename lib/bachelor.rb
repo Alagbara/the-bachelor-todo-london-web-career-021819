f78b53a73ff4dcfd8e5a0a4c298af56eb2bdfe75
Moses Olawoyin <m.bastford@gmail.com>
2019-02-14 03:22:21 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -59,7 +59,7 @@ def get_average_age_for_season(data, season)
   
     data[season].each do |values|
     binding.pry
-      value_hash.each do |info|
+      values.each do |info|
         age += info["age"].to_i
         counter +=1 
       
